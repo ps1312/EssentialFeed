2d09c6597c24db71fdb86439fda1bb23bbdf558b
Paulo Sérgio <ps131296@gmail.com>
2022-08-20 20:19:12 -0300

    assert RemoteFeedLoader.load makes request with HTTPClient

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -4,21 +4,42 @@ protocol HTTPClient {
     func get(url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    var requestedURL: URL?
+class RemoteFeedLoader {
+    private let httpClient: HTTPClient
 
-    func get(url: URL) {}
+    init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+    }
+
+    func load() {
+        httpClient.get(url: URL(string: "https://www.any-url.com")!)
+    }
 }
-
-class RemoteFeedLoader {}
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func testInitDoesNotMakeRequests() {
         let httpClientSpy = HTTPClientSpy()
-        let _ = RemoteFeedLoader()
+        let _ = RemoteFeedLoader(httpClient: httpClientSpy)
 
         XCTAssertNil(httpClientSpy.requestedURL)
     }
 
+    func testLoadMakesRequest() {
+        let httpClientSpy = HTTPClientSpy()
+        let sut = RemoteFeedLoader(httpClient: httpClientSpy)
+
+        sut.load()
+
+        XCTAssertNotNil(httpClientSpy.requestedURL)
+    }
+
+}
+
+class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+
+    func get(url: URL) {
+        requestedURL = url
+    }
 }
