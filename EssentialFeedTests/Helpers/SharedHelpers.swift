0ab4e8629aff653728ffc495f846beb27e8446e9
Paulo Sérgio <ps131296@gmail.com>
2022-08-23 04:43:58 -0300

    moved testMemoryLeak extension helper to its own file + extracted helpers to a separate folder

---
+++ EssentialFeedTests/Helpers/SharedHelpers.swift
@@ -0,0 +1,16 @@
+import Foundation
+import XCTest
+
+func makeError() -> NSError {
+    return NSError(domain: "Test", code: 1)
+}
+
+func makeURL() -> URL {
+    return URL(string: "https://www.a-url.com")!
+}
+
+func makeData() -> Data {
+    return Data("{}".utf8)
+}
+
+
--- EssentialFeedTests/Helpers/XCTestCase+testMemoryLeak.swift
+++ EssentialFeedTests/Helpers/XCTestCase+testMemoryLeak.swift
@@ -1,17 +1,4 @@
-import Foundation
 import XCTest
-
-func makeError() -> NSError {
-    return NSError(domain: "Test", code: 1)
-}
-
-func makeURL() -> URL {
-    return URL(string: "https://www.a-url.com")!
-}
-
-func makeData() -> Data {
-    return Data("{}".utf8)
-}
 
 extension XCTestCase {
     func testMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
