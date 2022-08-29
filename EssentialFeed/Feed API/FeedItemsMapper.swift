22995d693f8d2769f2998b1ea91dbf7fdd718c19
Paulo Sérgio <ps131296@gmail.com>
2022-08-29 08:28:32 -0300

    add OK_200 property to FeedItemsMapper in order to remove "magic number"

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -1,8 +1,10 @@
 import Foundation
 
 class FeedItemsMapper {
+    private static let OK_200 = 200
+
     static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
-        guard response.statusCode == 200 else { throw RemoteFeedLoader.Error.invalidData }
+        guard response.statusCode == OK_200 else { throw RemoteFeedLoader.Error.invalidData }
 
         do {
             let root = try JSONDecoder().decode(Root.self, from: data)
