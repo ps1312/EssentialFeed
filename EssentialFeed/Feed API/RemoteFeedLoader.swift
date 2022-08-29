9186cfade6c4f3d194357f2be2ef3c2b3988b241
Paulo Sérgio <ps131296@gmail.com>
2022-08-29 08:27:41 -0300

    add RemoteFeedItem DTO to remove the FeedItemsMapper coupling with FeedItem

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -22,7 +22,12 @@ public final class RemoteFeedLoader: FeedLoader {
 
             switch (result) {
             case let .success((data, response)):
-                completion(FeedItemsMapper.map(data, from: response))
+                do {
+                    let items = try FeedItemsMapper.map(data, from: response)
+                    completion(.success(items.toModels()))
+                } catch {
+                    completion(.failure(error))
+                }
                 break
             case .failure:
                 completion(.failure(Error.connectivity))
@@ -31,3 +36,8 @@ public final class RemoteFeedLoader: FeedLoader {
     }
 }
 
+private extension Array where Element == RemoteFeedItem {
+    func toModels() -> [FeedItem] {
+        return map { FeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image) }
+    }
+}
