a1e90650353341796c2d20d939a8026e4e0c134e
Paulo Sérgio <ps131296@gmail.com>
2022-08-30 17:02:41 -0300

    make FeedCachePolicy static since it doesn't keep any state

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -1,10 +1,13 @@
 import Foundation
 
 public final class FeedCachePolicy {
-    private let calendar = Calendar(identifier: .gregorian)
+    private init () {}
 
-    let maxCacheAgeInDays = 7
-    func validate(_ timestamp: Date, against date: Date) -> Bool {
+    private static let calendar = Calendar(identifier: .gregorian)
+
+    static let maxCacheAgeInDays = 7
+
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
 
         return date < maxCacheAge
@@ -13,8 +16,6 @@ public final class FeedCachePolicy {
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
-    private let calendar = Calendar(identifier: .gregorian)
-    private let cachePolicy = FeedCachePolicy()
 
     public init(store: FeedStore, currentDate: @escaping () -> Date = Date.init) {
         self.store = store
@@ -56,7 +57,7 @@ extension LocalFeedLoader {
             case let .failure(error):
                 completion(.failure(error))
 
-            case let .found(localFeed, timestamp) where self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(localFeed, timestamp) where FeedCachePolicy.validate(timestamp, against: self.currentDate()):
                 completion(.success(localFeed.toModels()))
 
             case .found, .empty:
@@ -76,7 +77,7 @@ extension LocalFeedLoader {
             case .failure:
                 self.store.delete { _ in }
 
-            case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(_, timestamp) where !FeedCachePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.delete { _ in }
 
             case .found, .empty:
