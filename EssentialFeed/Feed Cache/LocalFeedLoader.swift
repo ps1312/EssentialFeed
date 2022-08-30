d54faa96ecaf9564e5a1c81d2ecc3db840c67764
Paulo Sérgio <ps131296@gmail.com>
2022-08-30 19:51:23 -0300

    make LocalFeedLoader conform to FeedLoader protocol

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -33,7 +33,7 @@ extension LocalFeedLoader {
     }
 }
 
-extension LocalFeedLoader {
+extension LocalFeedLoader: FeedLoader {
     public typealias LoadResult = (LoadFeedResult) -> Void
 
     public func load(completion: @escaping LoadResult) {
