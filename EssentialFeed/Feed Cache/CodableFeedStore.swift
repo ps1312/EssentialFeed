e1085dd5c7ba95a9e974d810598f72767d92da2b
Paulo Sérgio <ps131296@gmail.com>
2022-08-31 16:23:11 -0300

    add custom queue in CodableFeedStore to permit concurrent serialised execution

--- EssentialFeed/Feed Cache/CodableFeedStore.swift
+++ EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -34,40 +34,53 @@ public final class CodableFeedStore: FeedStore {
     }
 
     private let storeURL: URL
+    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
 
     public init(storeURL: URL) {
         self.storeURL = storeURL
     }
 
-    public func retrieve(completion: (CacheRetrieveResult) -> Void) {
-        guard let data = try? Data(contentsOf: storeURL) else { return completion(.empty) }
+    public func retrieve(completion: @escaping (CacheRetrieveResult) -> Void) {
+        let storeURL = self.storeURL
 
-        do {
-            let cache = try JSONDecoder().decode(Cache.self, from: data)
-            completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
-        } catch {
-            completion(.failure(error))
+        queue.async {
+            guard let data = try? Data(contentsOf: storeURL) else { return completion(.empty) }
+
+            do {
+                let cache = try JSONDecoder().decode(Cache.self, from: data)
+                completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+            } catch {
+                completion(.failure(error))
+            }
         }
     }
 
-    public func persist(images: [LocalFeedImage], timestamp: Date, completion: PersistCompletion) {
-        do {
-            let encoded = try JSONEncoder().encode(Cache(localFeed: images, timestamp: timestamp))
-            try encoded.write(to: storeURL)
-            completion(nil)
-        } catch {
-            completion(error)
+    public func persist(images: [LocalFeedImage], timestamp: Date, completion: @escaping PersistCompletion) {
+        let storeURL = self.storeURL
+
+        queue.async(flags: .barrier) {
+            do {
+                let encoded = try JSONEncoder().encode(Cache(localFeed: images, timestamp: timestamp))
+                try encoded.write(to: storeURL)
+                completion(nil)
+            } catch {
+                completion(error)
+            }
         }
     }
 
-    public func delete(completion: DeletionCompletion) {
-        guard FileManager.default.fileExists(atPath: storeURL.path) else { return completion(nil) }
+    public func delete(completion: @escaping DeletionCompletion) {
+        let storeURL = self.storeURL
 
-        do {
-            try FileManager.default.removeItem(at: storeURL)
-            completion(nil)
-        } catch {
-            completion(error)
+        queue.async(flags: .barrier) {
+            guard FileManager.default.fileExists(atPath: storeURL.path) else { return completion(nil) }
+
+            do {
+                try FileManager.default.removeItem(at: self.storeURL)
+                completion(nil)
+            } catch {
+                completion(error)
+            }
         }
     }
 
