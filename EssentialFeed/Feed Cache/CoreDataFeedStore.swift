b43a0e7f1dca672463dda1f617a777882ac6bd1f
Paulo Sérgio <ps131296@gmail.com>
2022-09-02 17:29:28 -0300

    add NSPersistentContainer setup on `CoreDataFeedStore` initialisation

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -1,7 +1,37 @@
 import Foundation
+import CoreData
 
 public class CoreDataFeedStore: FeedStore {
-    public init() {}
+
+    private let container: NSPersistentContainer
+    private let context: NSManagedObjectContext
+
+    public init() {
+        let bundle = Bundle(for: CoreDataFeedStore.self)
+
+        guard let modelURL = bundle.url(forResource: "FeedStore", withExtension: "momd") else {
+            fatalError("Failed to find data model")
+        }
+
+        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Failed to create model from file: \(modelURL)")
+        }
+
+        container = NSPersistentContainer(name: "FeedStore", managedObjectModel: mom)
+
+        var loadStoresErrors = [Error]()
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                loadStoresErrors.append(error)
+            }
+        }
+
+        if !loadStoresErrors.isEmpty {
+            fatalError("Failed to load persistent stores")
+        }
+
+        context = container.newBackgroundContext()
+    }
 
     public func delete(completion: @escaping DeletionCompletion) {
         fatalError("Not implemented yet!")
