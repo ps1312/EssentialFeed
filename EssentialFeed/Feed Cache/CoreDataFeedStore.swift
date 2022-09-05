3be77cca8b553a8320a31caff97fc9f68afeb3f8
Paulo Sérgio <ps131296@gmail.com>
2022-09-05 14:05:58 -0300

    Simplify retrieve method by extracting the `ManagedFeedImage` to `LocalFeedImage` mapping to the `ManagedCache` scope

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -73,13 +73,7 @@ public class CoreDataFeedStore: FeedStore {
         context.perform { [context] in
             do {
                 if let cache = try context.fetch(request).first {
-                    let local = cache.feed.compactMap {
-                        ($0 as? ManagedFeedImage).map {
-                            LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
-                        }
-                    }
-
-                    completion(.found(feed: local, timestamp: cache.timestamp))
+                    completion(.found(feed: cache.locals, timestamp: cache.timestamp))
                 } else {
                     completion(.empty)
                 }
@@ -93,6 +87,14 @@ public class CoreDataFeedStore: FeedStore {
 final class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
+
+    var locals: [LocalFeedImage] {
+        return feed.compactMap {
+            ($0 as? ManagedFeedImage).map {
+                LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+            }
+        }
+    }
 }
 
 @objc(ManagedFeedImage)
