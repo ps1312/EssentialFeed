a44c5e4dcd580f54a12c3e25ef1145bc5282a320
Paulo Sérgio <ps131296@gmail.com>
2022-09-05 13:59:51 -0300

    map `ManagedFeedImage` to `LocalFeedImage` in a single step (instead of iterating the sequence twice)

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -73,9 +73,11 @@ public class CoreDataFeedStore: FeedStore {
         context.perform { [context] in
             do {
                 if let cache = try context.fetch(request).first {
-                    let local = cache.feed
-                        .compactMap { $0 as? ManagedFeedImage }
-                        .map { LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url) }
+                    let local = cache.feed.compactMap {
+                        ($0 as? ManagedFeedImage).map {
+                            LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+                        }
+                    }
 
                     completion(.found(feed: local, timestamp: cache.timestamp))
                 } else {
