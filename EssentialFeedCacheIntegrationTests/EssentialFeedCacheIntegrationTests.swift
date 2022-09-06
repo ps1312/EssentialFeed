daba593a05b0fcc2695d6e5f16f8512813af7b12
Paulo Sérgio <ps131296@gmail.com>
2022-09-06 11:44:05 -0300

    Ensure `LocalFeedLoader` and `CoreDataFeedStore` delivers cached values on non empty cache

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -3,4 +3,43 @@ import EssentialFeed
 
 class EssentialFeedCacheIntegrationTests: XCTestCase {
 
+    func test_LocalFeedLoaderAndCoreDataFeedStore_deliversCachedValuesOnNonEmptyCache() {
+        let exp = expectation(description: "Wait for save and load to complete")
+
+        let sut = makeSUT()
+
+        let images = uniqueImages()
+
+        sut.save(feed: images.models) { error in
+            sut.load { receivedResult in
+                switch (receivedResult) {
+                case .success(let feedImages):
+                    XCTAssertEqual(feedImages, images.models)
+
+                default:
+                    XCTFail("Expected success retrieving recent cached values, instead got \(receivedResult)")
+                }
+
+                exp.fulfill()
+            }
+        }
+
+        wait(for: [exp], timeout: 5.0)
+    }
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> LocalFeedLoader {
+        let storeURL = cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+        let coreDataFeedStore = try! CoreDataFeedStore(storeURL: storeURL)
+        let localFeedLoader = LocalFeedLoader(store: coreDataFeedStore)
+
+        testMemoryLeak(coreDataFeedStore, file: file, line: line)
+        testMemoryLeak(localFeedLoader, file: file, line: line)
+
+        return localFeedLoader
+    }
+
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+
 }
