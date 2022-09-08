02fde7b5dbc12098c4981d8d6b2237898c8053c5
Paulo Sérgio <ps131296@gmail.com>
2022-09-08 19:45:05 -0300

    Make `FeedLoader` public and ensure `FeedViewController` loads feed on viewDidLoad

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -1,18 +1,44 @@
 import XCTest
+import EssentialFeed
 
-class FeedViewController {}
+class FeedViewController: UIViewController {
+    private var loader: FeedLoader?
+
+    convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+
+    override func viewDidLoad() {
+        loader?.load { _ in }
+    }
+
+}
 
 class FeedViewControllerTests: XCTestCase {
 
     func test_init_doesLoadFeed() {
         let loader = FeedLoaderSpy()
-        let _ = FeedViewController()
+        let _ = FeedViewController(loader: loader)
 
         XCTAssertEqual(loader.loadCallsCount, 0)
     }
 
-    class FeedLoaderSpy {
+    func test_viewDidLoad_loadsFeed() {
+        let loader = FeedLoaderSpy()
+        let sut = FeedViewController(loader: loader)
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(loader.loadCallsCount, 1)
+    }
+
+    class FeedLoaderSpy: FeedLoader {
         var loadCallsCount = 0
+
+        func load(completion: @escaping (LoadFeedResult) -> Void) {
+            loadCallsCount += 1
+        }
     }
 
 }
