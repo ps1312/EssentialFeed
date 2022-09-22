ab291a620121a506548732b736b88d752636197b
Paulo Sérgio <ps131296@gmail.com>
2022-09-22 10:40:59 -0300

    Extracted `FeedImageCell` test helpers to a separate file

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -337,44 +337,6 @@ class FeedViewControllerTests: XCTestCase {
 
 }
 
-private extension FeedImageCell {
-    var descriptionText: String? {
-        return descriptionLabel.text
-    }
-
-    var locationText: String? {
-        return locationLabel.text
-    }
-
-    var isLocationHidden: Bool {
-        return locationContainer.isHidden
-    }
-
-    var isDescriptionHidden: Bool {
-        return descriptionLabel.isHidden
-    }
-
-    var isShowingLoadingIndicator: Bool {
-        return imageContainer.isShimmering
-    }
-
-    var isShowingRetryButton: Bool {
-        return !retryButton.isHidden
-    }
-
-    var feedImageData: Data? {
-        return feedImageView.image?.pngData()
-    }
-
-    func simulateImageLoadRetry() {
-        retryButton.allTargets.forEach { target in
-            retryButton.actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
-    }
-}
-
 private extension UIImage {
     static func make(withColor color: UIColor) -> UIImage {
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
