6f501dabbb20fea31042bfb0ec185294d76cfca5
Paulo Sérgio <ps131296@gmail.com>
2022-09-25 20:32:39 -0300

    Move `MainQueueDispatchDecorator` main thread check and handling to the main type (was duplicated in each extension)

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -29,32 +29,25 @@ class MainQueueDispatchDecorator<T> {
     init(decoratee: T) {
         self.decoratee = decoratee
     }
+
+    func dispatch(_ completion: @escaping () -> Void) {
+        guard Thread.isMainThread else { return DispatchQueue.main.async { completion() } }
+        completion()
+    }
 }
 
 extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
     func load(completion: @escaping (LoadFeedResult) -> Void) {
-        decoratee.load { result in
-            if Thread.isMainThread {
-                completion(result)
-            } else {
-                DispatchQueue.main.async {
-                    completion(result)
-                }
-            }
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
         }
     }
 }
 
 extension MainQueueDispatchDecorator: FeedImageLoader where T == FeedImageLoader {
     func load(from url: URL, completion: @escaping (FeedImageLoader.Result) -> Void) -> FeedImageLoaderTask {
-        decoratee.load(from: url) { result in
-            if Thread.isMainThread {
-                completion(result)
-            } else {
-                DispatchQueue.main.async {
-                    completion(result)
-                }
-            }
+        decoratee.load(from: url) { [weak self] result in
+            self?.dispatch { completion(result) }
         }
     }
 }
