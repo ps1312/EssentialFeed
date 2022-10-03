8467571aa90ac3229caa21f908ce7cc6b44dc2b8
Paulo Sérgio <ps131296@gmail.com>
2022-10-03 09:36:29 -0300

    Moved `HTTPClientSpy` to a separate `Helpers` group (inside `Feed API` test folder)

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCase.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCase.swift
@@ -148,27 +148,4 @@ class LoadFeedFromRemoteUseCase: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
 
-    private class HTTPClientSpy: HTTPClient {
-        var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
-        var requestedURLs: [URL] { return messages.map { $0.url } }
-
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-            messages.append((url, completion))
-        }
-
-        func completeWith(error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-
-        func completeWith(statusCode: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(
-                url: messages[index].url,
-                statusCode: statusCode,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            messages[index].completion(.success((data, response)))
-        }
-    }
-
 }
