0c7a7aa81eaf62649f434994236dd56cd2a0ac5d
Paulo Sérgio <ps131296@gmail.com>
2022-10-07 11:23:17 -0300

    `CacheFeedImageDecorator` delivers error when decoratee load fails

--- EssentialApp/EssentialAppTests/CacheFeedImageDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/CacheFeedImageDecoratorTests.swift
@@ -21,20 +21,41 @@ class CacheFeedImageDecoratorTests: XCTestCase, FeedLoaderTestCase {
         let loaderSpy = FeedImageLoaderSpy()
         let sut = CacheFeedImageDecorator(imageLoader: loaderSpy)
 
+        expect(sut, toCompleteWith: .success(data), when: {
+            loaderSpy.completeWith(data: data)
+        })
+    }
+
+    func test_load_deliversErrorWhenImageLoadFails() {
+        let error = makeNSError()
+        let loaderSpy = FeedImageLoaderSpy()
+        let sut = CacheFeedImageDecorator(imageLoader: loaderSpy)
+
+        expect(sut, toCompleteWith: .failure(error), when: {
+            loaderSpy.completeWith(error: error)
+        })
+    }
+
+    private func expect(_ sut: FeedImageLoader, toCompleteWith expectedResult: FeedImageLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "wait for image load to complete")
+
         _ = sut.load(from: makeURL()) { receivedResult in
-            switch (receivedResult) {
-            case .success(let receivedData):
-                XCTAssertEqual(receivedData, data)
+            switch (receivedResult, expectedResult) {
+            case let (.failure(receivedFailure), .failure(expectedFailure)):
+                XCTAssertEqual(receivedFailure as NSError, expectedFailure as NSError, file: file, line: line)
+
+            case let (.success(receivedData), .success(expectedData)):
+                XCTAssertEqual(receivedData, expectedData, file: file, line: line)
 
             default:
-                XCTFail("Expected image load to succeed, instead got \(receivedResult)")
+                XCTFail("Expected load to succeed, instead got \(receivedResult)", file: file, line: line)
 
             }
+
             exp.fulfill()
         }
 
-        loaderSpy.completeWith(data: data)
+        action()
 
         wait(for: [exp], timeout: 1.0)
     }
@@ -53,6 +74,10 @@ class CacheFeedImageDecoratorTests: XCTestCase, FeedLoaderTestCase {
 
         func completeWith(data: Data, at index: Int = 0) {
             completions[index](.success(data))
+        }
+
+        func completeWith(error: Error, at index: Int = 0) {
+            completions[index](.failure(error))
         }
 
     }
