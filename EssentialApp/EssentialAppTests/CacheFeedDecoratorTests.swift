ab3d65a9511d09a35e3618716d44c1f52c5f0b2c
Paulo Sérgio <ps131296@gmail.com>
2022-10-07 10:24:07 -0300

    `CacheFeedDecorator` delivers result when decoratee succeeds (feed is loaded with success)

---
+++ EssentialApp/EssentialAppTests/CacheFeedDecoratorTests.swift
@@ -0,0 +1,61 @@
+import XCTest
+import EssentialFeed
+
+class CacheFeedDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+
+    init(decoratee: FeedLoader) {
+        self.decoratee = decoratee
+    }
+
+    func load(completion: @escaping (LoadFeedResult) -> Void) {
+        decoratee.load(completion: completion)
+    }
+
+}
+
+class CacheFeedDecoratorTests: XCTestCase {
+
+    func test_load_deliversFeedWhenFeedLoaderSucceeds() {
+        let feed = uniqueFeed()
+        let loader = FeedLoaderSpy()
+        let sut = CacheFeedDecorator(decoratee: loader)
+
+        let exp = expectation(description: "wait for feed to load")
+        sut.load { receivedResult in
+            switch (receivedResult) {
+            case .success(let receivedFeed):
+                XCTAssertEqual(receivedFeed, feed)
+
+            default:
+                XCTFail("Expected feed load to succeed, instead got \(receivedResult)")
+            }
+
+            exp.fulfill()
+        }
+
+        loader.completeFeedLoad(with: feed)
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    private func uniqueFeed() -> [FeedImage] {
+        let url = URL(string: "https://www.any-url.com")!
+        let feedImage1 = FeedImage(id: UUID(), description: nil, location: nil, url: url)
+        let feedImage2 = FeedImage(id: UUID(), description: nil, location: nil, url: url)
+        return [feedImage1, feedImage2]
+    }
+
+    private final class FeedLoaderSpy: FeedLoader {
+        var completions = [(LoadFeedResult) -> Void]()
+
+        func load(completion: @escaping (LoadFeedResult) -> Void) {
+            completions.append(completion)
+        }
+
+        func completeFeedLoad(with feed: [FeedImage], at index: Int = 0) {
+            completions[index](.success(feed))
+        }
+
+    }
+
+}
