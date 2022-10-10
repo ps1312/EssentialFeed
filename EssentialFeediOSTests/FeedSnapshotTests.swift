53290b749d7c513e8e27957ed6fd77787a4854b8
Paulo Sérgio <ps131296@gmail.com>
2022-10-10 14:08:43 -0300

    add record of feed with content

--- EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import EssentialFeed
 import EssentialFeediOS
 
 class FeedSnapshotTests: XCTestCase {
@@ -11,6 +12,14 @@ class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), name: "EMPTY_FEED")
     }
 
+    func test_nonEmptyFeed() {
+        let sut = makeSUT()
+
+        sut.cellControllers = nonEmptyFeed()
+
+        record(snapshot: sut.snapshot(), name: "FEED_WITH_CONTENT")
+    }
+
     private func makeSUT() -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
@@ -21,6 +30,29 @@ class FeedSnapshotTests: XCTestCase {
 
     private func emptyFeed() -> [FeedImageCellController] {
         return []
+    }
+
+    private func nonEmptyFeed() -> [FeedImageCellController] {
+        let cellController1 = makeImageCellController(
+            image: UIImage.make(withColor: .orange),
+            description: "Mount Everest 🏔 is Earth's highest mountain above sea level, located in the Mahalangur Himal sub-range of the Himalayas. The China–Nepal border runs across its summit point. Its elevation of 8,848.86 m was most recently established in 2020 by the Chinese and Nepali authorities.",
+            location: "Solukhumbu District, Province No. 1\nNepal"
+        )
+
+        let cellController2 = makeImageCellController(
+            image: UIImage.make(withColor: .magenta),
+            description: nil,
+            location: nil
+        )
+        return [cellController1, cellController2]
+    }
+
+    private func makeImageCellController(image: UIImage?, description: String?, location: String?) -> FeedImageCellController {
+        let delegate = FeedImageCellControllerDelegateStub(image: image, description: description, location: location)
+        let controller = FeedImageCellController(delegate: delegate)
+        delegate.controller = controller
+
+        return controller
     }
 
     private func record(snapshot: UIImage, name: String, file: StaticString = #file, line: UInt = #line) {
@@ -37,6 +69,21 @@ class FeedSnapshotTests: XCTestCase {
         } catch {
             XCTFail("Failed to record snapshot image PNG", file: file, line: line)
         }
+    }
+
+    private class FeedImageCellControllerDelegateStub: FeedImageCellControllerDelegate {
+        private let stubbedResult: FeedImageViewModel<UIImage>
+        weak var controller: FeedImageCellController?
+
+        init (image: UIImage?, description: String?, location: String?) {
+            self.stubbedResult = FeedImageViewModel(isLoading: false, shouldRetry: image == nil, image: image, description: description, location: location)
+        }
+
+        func didRequestImageLoad() {
+            controller?.display(stubbedResult)
+        }
+
+        func didCancelImageLoad() {}
     }
 }
 
