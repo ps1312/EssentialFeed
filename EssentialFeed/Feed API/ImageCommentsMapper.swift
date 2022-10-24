ca94922e73f338af3932efde7e4e1d1a13605a25
Paulo Sérgio <ps131296@gmail.com>
2022-10-24 09:32:37 -0300

    Refactor decoding strategy in `ImageCommentsMapper`

--- EssentialFeed/Feed API/ImageCommentsMapper.swift
+++ EssentialFeed/Feed API/ImageCommentsMapper.swift
@@ -1,14 +1,35 @@
 import Foundation
 
 class ImageCommentsMapper {
-    private static let OK_200 = 200
+    private struct RemoteImageCommentsBody: Decodable {
+        struct RemoteImageComment: Decodable {
+            struct RemoteAuthor: Decodable {
+                let username: String
+            }
+
+            let id: UUID
+            let message: String
+            let created_at: Date
+            let author: RemoteAuthor
+        }
+
+        let items: [RemoteImageComment]
+
+        func toModels() -> [ImageComment] {
+            items.map { ImageComment(id: $0.id, message: $0.message, createdAt: $0.created_at, author: $0.author.username) }
+        }
+    }
 
     static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
         guard isOK(response) else { throw RemoteImageCommentsLoader.Error.invalidData }
 
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+
         do {
-            let root = try JSONDecoder().decode(Root.self, from: data)
-            return try root.items.toModels()
+            let body = try decoder.decode(RemoteImageCommentsBody.self, from: data)
+            return body.toModels()
+
         } catch {
             throw RemoteImageCommentsLoader.Error.invalidData
         }
@@ -16,39 +37,5 @@ class ImageCommentsMapper {
 
     private static func isOK(_ response: HTTPURLResponse) -> Bool {
         (200...299).contains(response.statusCode)
-    }
-}
-
-private struct RemoteImageComment: Decodable {
-    struct RemoteAuthor: Decodable {
-        let username: String
-    }
-
-    let id: UUID
-    let message: String
-    let created_at: String
-    let author: RemoteAuthor
-}
-
-private struct Root: Decodable {
-    var items = [RemoteImageComment]()
-}
-
-private extension Array where Element == RemoteImageComment {
-    func toModels() throws -> [ImageComment] {
-        let formatter = ISO8601DateFormatter()
-
-        return try map {
-            guard let date = formatter.date(from: $0.created_at) else {
-                throw RemoteImageCommentsLoader.Error.invalidData
-            }
-
-            return ImageComment(
-                id: $0.id,
-                message: $0.message,
-                createdAt: date,
-                author: $0.author.username
-            )
-        }
     }
 }
