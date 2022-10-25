cb0eeb04fabd87edb7c8332941f5a997fd4f09b5
Paulo Sérgio <ps131296@gmail.com>
2022-10-25 13:44:27 -0300

    duplicated `FeedPresenter` + tests into `LoadResourcePresenter` (a generic type for presenting resources loading, success and failure)

---
+++ EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+public final class LoadResourcePresenter {
+    private let loadingView: FeedLoadingView
+    private let feedView: FeedView
+    private let errorView: FeedErrorView
+
+    public static var title: String {
+        NSLocalizedString(
+            "FEED_VIEW_TITLE",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "The feed view screen title"
+        )
+    }
+
+    public static var loadError: String {
+        NSLocalizedString(
+            "FEED_VIEW_CONNECTION_ERROR",
+            tableName: "Feed",
+            bundle: Bundle(for: FeedPresenter.self),
+            comment: "The error message for feed load failure"
+        )
+    }
+
+    public init(loadingView: FeedLoadingView, feedView: FeedView, errorView: FeedErrorView) {
+        self.loadingView = loadingView
+        self.feedView = feedView
+        self.errorView = errorView
+    }
+
+    public func didStartLoadingFeed() {
+        errorView.display(FeedErrorViewModel.noError)
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+
+    public func didLoadFeed(_ feed: [FeedImage]) {
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+        feedView.display(FeedViewModel(feed: feed))
+    }
+
+    public func didFinishLoadingFeedWithError() {
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+        errorView.display(FeedErrorViewModel.error(message: FeedPresenter.loadError))
+    }
+}
