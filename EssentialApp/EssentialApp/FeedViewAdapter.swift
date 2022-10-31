d86957de83145ac886eca4919dce4d076243462d
Paulo Sérgio <ps131296@gmail.com>
2022-10-31 09:48:59 -0300

    removing comments

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -12,22 +12,18 @@ final class FeedViewAdapter: ResourceView {
 
     func display(_ viewModel: FeedViewModel) {
         feedController?.cellControllers = viewModel.feed.map { model in
-            // abstracts the communication between domain and presentation (by *adapting the image load output to the presenter input)
             let cellControllerDelegate = FeedImageLoadPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(
                 model: model,
                 imageLoader: imageLoader
             )
 
-            // configures cell and requests for image load through a protocol
             let feedImageView = FeedImageCellController(delegate: cellControllerDelegate)
 
-            // present views by using display() methods
             let feedImagePresenter = FeedImagePresenter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(
                 feedImageView: WeakRefVirtualProxy(feedImageView),
                 imageTransformer: UIImage.init
             )
 
-            // handle composition details...
             cellControllerDelegate.presenter = feedImagePresenter
 
             return feedImageView
