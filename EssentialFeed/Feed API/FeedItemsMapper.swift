dc702844879b8b6a6d7964b2686b1195f0791cb6
Paulo Sérgio <ps131296@gmail.com>
2022-11-07 10:48:58 -0300

    fix `FeedItemsMapper` to return all mapped items

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -22,7 +22,7 @@ public class FeedItemsMapper {
 
         do {
             let root = try JSONDecoder().decode(Root.self, from: data)
-            return [root.images[0], root.images[1], root.images[2]]
+            return root.images
         } catch {
             throw MapError()
         }
