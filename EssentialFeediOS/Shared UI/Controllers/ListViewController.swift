9365f3a990b2150e1456642ccaed9a944e789f39
Paulo Sérgio <ps131296@gmail.com>
2022-11-11 09:41:30 -0300

    removing unnecessary `loadingControllers` from generic list view controller

--- EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -4,15 +4,14 @@ import EssentialFeed
 public final class ListViewController: UITableViewController, UITableViewDataSourcePrefetching, ResourceLoadingView, ResourceErrorView {
     private lazy var dataSource: UITableViewDiffableDataSource<Int, CellController> = {
         .init(tableView: tableView) { [weak self] tableView, indexPath, controller in
-            self?.cellController(at: indexPath).dataSource.tableView(tableView, cellForRowAt: indexPath)
+            controller.dataSource.tableView(tableView, cellForRowAt: indexPath)
         }
     }()
-    private var loadingControllers = [IndexPath: CellController]()
 
     public lazy var errorView: ErrorButton = ErrorButton()
     public var onRefresh: (() -> Void)?
     public var cellControllers = [CellController]() {
-        didSet { handleCellControllersUpdate() }
+        didSet { onCellControllersUpdate() }
     }
 
     public override func viewDidLoad() {
@@ -53,7 +52,7 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
         tableView.tableHeaderView = container
     }
 
-    private func handleCellControllersUpdate() {
+    private func onCellControllersUpdate() {
         var snapshot = NSDiffableDataSourceSnapshot<Int, CellController>()
         snapshot.appendSections([0])
         snapshot.appendItems(cellControllers)
@@ -82,38 +81,26 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
     }
 
     public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let dl = cellController(at: indexPath).delegate
+        let dl = cellControllers[indexPath.row].delegate
         dl?.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
     }
 
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let dl = removeLoadingController(at: indexPath)?.delegate
+        let dl = cellControllers[indexPath.row].delegate
         dl?.tableView?(tableView, didEndDisplaying: cell, forRowAt: indexPath)
     }
 
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
-            let pf = cellController(at: indexPath).prefetch
+            let pf = cellControllers[indexPath.row].prefetch
             pf?.tableView(tableView, prefetchRowsAt: indexPaths)
         }
     }
 
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
-            let pf = removeLoadingController(at: indexPath)?.prefetch
+            let pf = cellControllers[indexPath.row].prefetch
             pf?.tableView?(tableView, cancelPrefetchingForRowsAt: indexPaths)
         }
-    }
-
-    private func cellController(at indexPath: IndexPath) -> CellController {
-        let controller = cellControllers[indexPath.row]
-        loadingControllers[indexPath] = controller
-        return controller
-    }
-
-    private func removeLoadingController(at indexPath: IndexPath) -> CellController? {
-        let controller = loadingControllers[indexPath]
-        loadingControllers[indexPath] = nil
-        return controller
     }
 }
