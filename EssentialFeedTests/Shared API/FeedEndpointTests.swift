34ece2b4329aa2168573487565e4ac0e2b1c67b1
Paulo Sérgio <ps131296@gmail.com>
2022-11-23 14:07:29 -0300

    ensure `FeedEndpoint` appends image UUID in "after" when FeedImage is passed as param

--- EssentialFeedTests/Shared API/FeedEndpointTests.swift
+++ EssentialFeedTests/Shared API/FeedEndpointTests.swift
@@ -1,16 +1,20 @@
 import XCTest
+import EssentialFeed
 
 enum FeedEndpoint {
-    case get
+    case get(after: FeedImage?)
 
     func url(baseURL: URL) -> URL {
         switch self {
-        case .get:
+        case let .get(feedImage):
             var component = URLComponents()
             component.scheme = baseURL.scheme
             component.host = baseURL.host
             component.path = baseURL.path + "/v1/feed"
-            component.query = "limit=10"
+            component.queryItems = [
+                feedImage.map { _ in URLQueryItem(name: "after", value: feedImage?.id.uuidString) },
+                URLQueryItem(name: "limit", value: "10")
+            ].compactMap { $0 }
             return component.url!
         }
     }
@@ -19,11 +23,19 @@ enum FeedEndpoint {
 class FeedEndpointTests: XCTestCase {
     func test_get_deliversFeedEndpoint() {
         let baseURL = URL(string: "http://base-url.com")!
-        let received = FeedEndpoint.get.url(baseURL: baseURL)
+        let received = FeedEndpoint.get(after: nil).url(baseURL: baseURL)
 
         XCTAssertEqual(received.scheme, "http", "scheme")
         XCTAssertEqual(received.host, "base-url.com", "host")
         XCTAssertEqual(received.path, "/v1/feed", "path")
         XCTAssertEqual(received.query, "limit=10", "query")
+    }
+
+    func test_getWithImage_appendsQueryToEndpoint() {
+        let image = uniqueImage()
+        let baseURL = URL(string: "http://base-url.com")!
+        let received = FeedEndpoint.get(after: image).url(baseURL: baseURL)
+
+        XCTAssertEqual(received.query?.contains("after=\(image.id.uuidString)"), true, "after")
     }
 }
