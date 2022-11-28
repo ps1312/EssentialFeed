0904471579daea3a9d85e1b115ea49af3a37fe38
Paulo Sérgio <ps131296@gmail.com>
2022-11-28 10:07:23 -0300

    `InMemoryFeedStore` delivers persisted cache when not empty

--- EssentialFeedTests/Feed Cache/InMemoryFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/InMemoryFeedStoreTests.swift
@@ -2,8 +2,19 @@ import XCTest
 import EssentialFeed
 
 class InMemoryFeedStore {
+    var cache = [LocalFeedImage]()
+
     func retrieve(completion: @escaping FeedStore.RetrieveCompletion) {
-        completion(.empty)
+        if cache.isEmpty {
+            completion(.empty)
+        } else {
+            completion(.found(feed: cache, timestamp: Date()))
+        }
+    }
+
+    func persist(images: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.PersistCompletion) {
+        cache = images
+        completion(nil)
     }
 }
 
@@ -24,5 +35,30 @@ class InMemoryFeedStoreTests: XCTestCase {
         }
 
         wait(for: [exp], timeout: 5.0)
+    }
+
+    func test_retrieveAfterPersist_deliversLastCachedImages() {
+        let locals = uniqueImages().locals
+
+        let now = Date()
+        let sut = InMemoryFeedStore()
+
+        let exp = expectation(description: "Wait for cache persistance")
+        sut.persist(images: locals, timestamp: now) { error in
+            exp.fulfill()
+        }
+
+        let exp1 = expectation(description: "Wait for cache retrieve")
+        sut.retrieve { received in
+            switch (received) {
+            case let .found(feed, _):
+                XCTAssertEqual(feed, locals)
+            default:
+                XCTFail("Expected retrieve to return .empty, got \(received)")
+            }
+
+            exp1.fulfill()
+        }
+        wait(for: [exp, exp1], timeout: 5.0)
     }
 }
