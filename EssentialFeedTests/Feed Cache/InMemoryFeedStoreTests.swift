bd5d45113ed6045646f23e351e1a340fdf630ea2
Paulo Sérgio <ps131296@gmail.com>
2022-11-28 10:22:52 -0300

    extracted `expect(toRetrieve:)` helper + improve coverage by also asserting the cache timestamp

--- EssentialFeedTests/Feed Cache/InMemoryFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/InMemoryFeedStoreTests.swift
@@ -2,13 +2,18 @@ import XCTest
 import EssentialFeed
 
 class InMemoryFeedStore {
+    private let currentDate: () -> Date
     var cache = [LocalFeedImage]()
+
+    init(currentDate: @escaping () -> Date = Date.init) {
+        self.currentDate = currentDate
+    }
 
     func retrieve(completion: @escaping FeedStore.RetrieveCompletion) {
         if cache.isEmpty {
             completion(.empty)
         } else {
-            completion(.found(feed: cache, timestamp: Date()))
+            completion(.found(feed: cache, timestamp: currentDate()))
         }
     }
 
@@ -21,44 +26,43 @@ class InMemoryFeedStore {
 class InMemoryFeedStoreTests: XCTestCase {
     func test_init_doesNotHaveSideEffects() {
         let sut = InMemoryFeedStore()
+        expect(sut, toRetrieve: .empty)
+    }
 
-        let exp = expectation(description: "Wait for cache retrieval")
-        sut.retrieve { received in
-            switch (received) {
-            case .empty:
+    func test_retrieveAfterPersist_deliversLastCachedImagesWithTimestamp() {
+        let now = Date()
+        let locals = uniqueImages().locals
+        let sut = InMemoryFeedStore(currentDate: { now })
+
+        let exp = expectation(description: "Wait for cache persistance")
+        sut.persist(images: locals, timestamp: now) { error in
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+
+        expect(sut, toRetrieve: .found(feed: locals, timestamp: now))
+    }
+
+    func expect(_ sut: InMemoryFeedStore, toRetrieve expectedResult: CacheRetrieveResult, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "wait for cache retrieval")
+
+        sut.retrieve { receivedResult in
+            switch(receivedResult, expectedResult) {
+            case (.empty, .empty):
                 break
+
+            case let (.found(receivedFeed, receivedTimestamp), .found(expectedFeed, expectedTimestamp)):
+                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
+                XCTAssertEqual(receivedTimestamp, expectedTimestamp, file: file, line: line)
+
             default:
-                XCTFail("Expected retrieve to return .empty, got \(received)")
+                XCTFail("Expected \(expectedResult), received \(receivedResult)")
+
             }
 
             exp.fulfill()
         }
 
         wait(for: [exp], timeout: 5.0)
-    }
-
-    func test_retrieveAfterPersist_deliversLastCachedImages() {
-        let locals = uniqueImages().locals
-
-        let now = Date()
-        let sut = InMemoryFeedStore()
-
-        let exp = expectation(description: "Wait for cache persistance")
-        sut.persist(images: locals, timestamp: now) { error in
-            exp.fulfill()
-        }
-
-        let exp1 = expectation(description: "Wait for cache retrieve")
-        sut.retrieve { received in
-            switch (received) {
-            case let .found(feed, _):
-                XCTAssertEqual(feed, locals)
-            default:
-                XCTFail("Expected retrieve to return .empty, got \(received)")
-            }
-
-            exp1.fulfill()
-        }
-        wait(for: [exp, exp1], timeout: 5.0)
     }
 }
